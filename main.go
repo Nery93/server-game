e45fe779c509dbec66a86c8fed88e6f11d7b3d10
Nery93 <guilherme.rp93@hotmail.com>
2026-07-22 20:22:37 +0100

    refactor: restructure server initialization and shutdown handling

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/seuusername/guess-game/internal/handler"
 	"github.com/seuusername/guess-game/internal/store"
@@ -20,6 +24,12 @@ func main() {
 	gameStore := store.NewStore()
 	createHandler := handler.NewHandler(gameStore)
 
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: nil, // Use the default HTTP mux
+	}
+
+	// Register the handler functions for the endpoints
 	http.HandleFunc("/game", createHandler.CreateGame)
 	http.HandleFunc("/game/{id}/guess", createHandler.GuessNumber)
 	http.HandleFunc("/game/{id}", createHandler.GetGameStatus)
@@ -27,8 +37,24 @@ func main() {
 	// Start the HTTP server
 	slog.Info("Server Started", "port", port)
 
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		slog.Error("Server failed to start", "error", err)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed to start", "error", err)
+		}
+	}()
+
+	// Wait for a termination signal to gracefully shut down the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	slog.Info("Shutdown signal received")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server forced to shutdown", "error", err)
+	} else {
+		slog.Info("Server shutdown gracefully")
 	}
 }
